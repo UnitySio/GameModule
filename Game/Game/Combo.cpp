7a4780ab8f3a6a72646f5566f710080757157b7f
UnitySio <dnwls010728@gmail.com>
2022-12-06 19:10:28 +0900

    style: 일부 코드 변경

--- Game/Game/Combo.cpp
+++ Game/Game/Combo.cpp
@@ -23,7 +23,7 @@ void Combo::Update()
 		timer_ = 0.f;
 	}
 
-	position_ = Vector2().Lerp(position_, { 620.f, 240.f }, DELTA_TIME * 100.f);
+	position_ = Vector2().Lerp(position_, { 600.f, 240.f }, DELTA_TIME * 60.f);
 	SetPosition(position_);
 }
 
@@ -34,11 +34,8 @@ void Combo::Render()
 	WCHAR word[1024];
 	wsprintf(word, L"%d COMBO", combo_);
 
-	SetTextColor(WINDOW->GetHDC(), RGB(0, 0, 0));
-
 	RECT rect = { render_position.x_ - 100, render_position.y_ - 20, render_position.x_ + 20, render_position.y_ };
-	Rectangle(WINDOW->GetHDC(), rect.left, rect.top, rect.right, rect.bottom);
-	DrawText(WINDOW->GetHDC(), word, -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+	DrawText(WINDOW->GetHDC(), word, -1, &rect, DT_SINGLELINE | DT_RIGHT | DT_VCENTER);
 }
 
 void Combo::AddCombo()
